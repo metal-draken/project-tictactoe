4e2ee80909090f22079d91fdc27f936e67bc7950
Marco Salas Diaz <86160295+metal-draken@users.noreply.github.com>
2021-10-29 13:23:49 -0500

    01 Enums... 03 to go

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,30 +1,30 @@
+const Marks = {
+  P1: "X",
+  P2: "O",
+  Empty: ".",
+};
+
 const players = ["player1", "player2"];
 const states = ["open", "player1won", "player2won", "draw"];
 const squareStates = ["not set", "player 1", "player 2"];
 
 class TicTacToe {
-  constructor() {
-    this.field = [
-      [".", ".", "."],
-      [".", ".", "."],
-      [".", ".", "."],
-    ];
-    this.currentPlayer = players[0];
-    this.currentState = states[0];
-    this.movesArray = [];
-  }
-
+  // Saninn's comment: Usually the functions should be defined AFTER the constructor... but ok...
   resetGame() {
     this.field = [
-      [".", ".", "."],
-      [".", ".", "."],
-      [".", ".", "."],
+      [Marks.Empty, Marks.Empty, Marks.Empty],
+      [Marks.Empty, Marks.Empty, Marks.Empty],
+      [Marks.Empty, Marks.Empty, Marks.Empty],
     ];
     this.currentPlayer = players[0];
     this.currentState = states[0];
   }
 
-  playerHasWon(userMark) {
+  constructor() {
+    this.resetGame();
+  }
+
+  _playerHasWon(userMark) {
     // returns boolean
     if (
       (this.field[0][0] === userMark) &
@@ -58,23 +58,23 @@ class TicTacToe {
     return false;
   }
 
-  gameState() {
-    if (this.playerHasWon("X")) {
+  _gameState() {
+    if (this._playerHasWon(Marks.P1)) {
       this.currentState = states[1];
       console.log("Player 1 has won.");
       return;
     }
 
-    if (this.playerHasWon("O")) {
+    if (this._playerHasWon(Marks.P2)) {
       this.currentState = states[2];
       console.log("Player 2 has won.");
       return;
     }
 
     if (
-      !this.field[0].includes(".") &
-      !this.field[1].includes(".") &
-      !this.field[2].includes(".")
+      !this.field[0].includes(Marks.Empty) &
+      !this.field[1].includes(Marks.Empty) &
+      !this.field[2].includes(Marks.Empty)
     ) {
       this.currentState = states[3];
       console.log("Draw.");
@@ -85,19 +85,19 @@ class TicTacToe {
     }*/
   }
 
-  move(a, b) {
-    if (this.field[a][b] == "X" || this.field[a][b] == "O") {
+  _move(a, b) {
+    if (this.field[a][b] == Marks.P1 || this.field[a][b] == Marks.P2) {
       console.log("This square is already occupied");
       console.log(this.currentPlayer);
       return "This square is already occupied";
     }
     if (this.currentPlayer == players[0]) {
-      this.field[a][b] = "X";
+      this.field[a][b] = Marks.P1;
       this.currentPlayer = players[1];
       console.table(this.field);
       console.log(this.currentPlayer);
     } else {
-      this.field[a][b] = "O";
+      this.field[a][b] = Marks.P2;
       this.currentPlayer = players[0];
       console.table(this.field);
       console.log(this.currentPlayer);
@@ -105,75 +105,58 @@ class TicTacToe {
   }
 
   selectUpLeft() {
-    const newState = this.move(0, 0);
-    this.gameState();
+    const newState = this._move(0, 0);
+    this._gameState();
     return newState;
   }
 
   selectUpCenter() {
-    const newState = this.move(0, 1);
-    this.gameState();
+    const newState = this._move(0, 1);
+    this._gameState();
     return newState;
   }
 
   selectUpRight() {
-    const newState = this.move(0, 2);
-    this.gameState();
+    const newState = this._move(0, 2);
+    this._gameState();
     return newState;
   }
 
   selectCenterLeft() {
-    const newState = this.move(1, 0);
-    this.gameState();
+    const newState = this._move(1, 0);
+    this._gameState();
     return newState;
   }
 
   selectCenterCenter() {
-    const newState = this.move(1, 1);
-    this.gameState();
+    const newState = this._move(1, 1);
+    this._gameState();
     return newState;
   }
 
   selectCenterRight() {
-    const newState = this.move(1, 2);
-    this.gameState();
+    const newState = this._move(1, 2);
+    this._gameState();
     return newState;
   }
 
   selectDownLeft() {
-    const newState = this.move(2, 0);
-    this.gameState();
+    const newState = this._move(2, 0);
+    this._gameState();
     return newState;
   }
 
   selectDownCenter() {
-    const newState = this.move(2, 1);
-    this.gameState();
+    const newState = this._move(2, 1);
+    this._gameState();
     return newState;
   }
 
   selectDownRight() {
-    const newState = this.move(2, 2);
-    this.gameState();
+    const newState = this._move(2, 2);
+    this._gameState();
     return newState;
   }
-
-  /*
-     moves(a) {
-         this.movesArray = [this.selectUpLeft(),
-            this.selectUpCenter(),
-            this.selectUpRight(),
-            this.selectCenterLeft(),
-            this.selectCenterCenter(),
-            this.selectCenterRight(),
-            this.selectDownLeft(),
-            this.selectDownCenter(),
-            this.selectDownRight()];
-        this.movesArray[a];
-    }
-    THIS DIDN'T WORK...
-
-    */
 }
 
 module.exports.TicTacToe = TicTacToe;
